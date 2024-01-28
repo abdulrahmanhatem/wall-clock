f8f257ffea499dcaae8325d2e23f6a8eb5a2decc
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2024-01-28 05:42:10 +0200

    create clock face via javascript rahther than html

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+
+let clockFace = document.querySelector(".clock-face");
+
+for (let n = 0; n < 12; n++) {
+    let number = document.createElement("span");
+    let deg = n *30; 
+
+    let docStyles = window.getComputedStyle(document.body);    
+    let clockFaceRadius = docStyles.getPropertyValue("--clock-face-radius");
+    let viewRatio = docStyles.getPropertyValue("--view-ratio");
+
+    // let unit = parseFloat(viewRatio)
+    // console.log(unit.match(/\D/)[0]);
+
+     console.log(clockFaceRadius * parseFloat(viewRatio));
+    let translate = `${50}%, ${clockFaceRadius * parseFloat(viewRatio)}rem`;
+
+    let line = document.createElement("span");
+    
+    line.classList.add("line");
+
+
+    // console.log(line.style.height);
+    line.style.rotate = `0 0 1 ${-deg}deg`;
+    line.style.top = `calc(4 * var(--view-ratio))`;
+    line.style.left = 0;
+
+
+
+
+
+
+    // line.style.top = `calc(${Math.sin(deg)} * var(--view-ratio))`;
+    // line.style.left = `calc(- var(--view-ratio))`;
+ 
+    number.appendChild(line);
+
+    
+    // number.style.rotate = `0 0 1 ${deg}deg`;
+    // number.style.translate = translate;
+
+    number.style.transform = `rotate(${deg}deg) translate(${translate})`;
+
+    clockFace.appendChild(number)
+}
+
+
+
 let audio = document.querySelector(".audio-option img");
 
 
