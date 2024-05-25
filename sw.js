9bef864d30e85aac6f8fa90f911a418d313ea8bf
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2024-05-25 07:50:28 +0300

    clean service worker file

--- sw.js
+++ sw.js
@@ -1,89 +1,86 @@
-const addResourcesToCache = async (resources) => {
-    const cache = await caches.open('v1');
-    await cache.addAll(resources);
-};
-  
-const putInCache = async (request, response) => {
-const cache = await caches.open('v1');
-await cache.put(request, response);
-};
-  
-const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
-    // First try to get the resource from the cache
-    const responseFromCache = await caches.match(request);
-    if (responseFromCache) {
-      return responseFromCache;
-    }
-  
-    // Next try to use the preloaded response, if it's there
-    // NOTE: Chrome throws errors regarding preloadResponse, see:
-    // https://bugs.chromium.org/p/chromium/issues/detail?id=1420515
-    // https://github.com/mdn/dom-examples/issues/145
-    // To avoid those errors, remove or comment out this block of preloadResponse
-    // code along with enableNavigationPreload() and the "activate" listener.
-    const preloadResponse = await preloadResponsePromise;
-    if (preloadResponse) {
-      console.info('using preload response', preloadResponse);
-      putInCache(request, preloadResponse.clone());
-      return preloadResponse;
-    }
-  
-    // Next try to get the resource from the network
+const staticCacheName = "v1";
+const dynamicCacheName = "dv1";
+
+const assets = [
+  "/",
+  "/index.html", 
+  "/fallback.html",
+  "/script.js", 
+  "/style.css", 
+  "/icons/volume.webp", 
+  "/icons/volume-slash.webp", 
+  "/audio/tick.mp3", 
+  "/audio/strike-twelve.mp3"
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(staticCacheName).then(async (cache) => {
+    let ok;
     try {
-      const responseFromNetwork = await fetch(request.clone());
-      // response may be used only once
-      // we need to save clone to put one copy in cache
-      // and serve second one
-      putInCache(request, responseFromNetwork.clone());
-      return responseFromNetwork;
-    } catch (error) {
-      const fallbackResponse = await caches.match(fallbackUrl);
-      if (fallbackResponse) {
-        return fallbackResponse;
+      ok = await cache.addAll(assets);
+    } catch (err) {
+      for (let i of assets) {
+        try {
+          ok = await cache.add(i);
+        } catch (err) {
+          console.warn('sw: cache.add',i);
+        }
       }
-      // when even the fallback response is not available,
-      // there is nothing we can do, but we must always
-      // return a Response object
-      return new Response('Network error happened', {
-        status: 408,
-        headers: { 'Content-Type': 'text/plain' },
-      });
     }
-};
-  
-const enableNavigationPreload = async () => {
-    if (self.registration.navigationPreload) {
-      // Enable navigation preloads!
-      await self.registration.navigationPreload.enable();
-    }
-};
-  
-self.addEventListener('activate', (event) => {
-    event.waitUntil(enableNavigationPreload());
+    return ok;
+  }));
+
+  console.log('ServiceWorker installed');
 });
+
+// activate service worker event 
+self.addEventListener("activate", event=>{
+  // console.log("Service worker is activated!");
+  event.waitUntil(
+      async () => {
+          
+      caches.keys().then(keys => {
+          return new Promise.all(
+              keys.filter(key => key !== staticCacheName && key !== dynamicCacheName).map(key => {
+                  caches.delete(key);
+              })
+          );
+      }).catch(e => console.log(e))
+      }
+  );
+})
   
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-      addResourcesToCache([
-        "/",
-        "/index.html", 
-        "/fallback.html",
-        "/script.js", 
-        "/style.css", 
-        "/icons/volume.svg", 
-        "/icons/volume-slash.svg", 
-        "/audio/tick.mp3", 
-        "/audio/strike-twelve.mp3"
-    ])
-    );
-});
-  
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      cacheFirst({
-        request: event.request,
-        preloadResponsePromise: event.preloadResponse,
-        fallbackUrl: '/fallback.html',
+
+
+// fetch service worker event
+self.addEventListener("fetch", event =>{
+  // console.log("fetch event", event );
+  event.respondWith(
+      caches.match(event.request)
+      .then(res => res || fetch(event.request).then(fetchRes => {
+        console.log("fetchRes", fetchRes);
+          return caches.open(dynamicCacheName).then(cache => {
+              cache.put(event.request.url , fetchRes);
+              limitCacheSize(dynamicCacheName, 15)
+              return fetchRes;
+          });
+      }))
+      .catch(() => {
+        console.log(event.request.url);
+          if (event.request.url.indexOf(".html") > -1) {
+              return caches.match("/fallback.html");
+          }
       })
-    );
-});
+  );
+})
+
+// cache zise limit function 
+const limitCacheSize = (name, size) =>{
+  caches.open(name).then(cache => {
+      cache.keys().then(keys => {
+          if (keys.length > size) {
+              cache.delete(keys[0]).then(limitCacheSize(name, size));
+          }
+      }).catch(e => console.log(e))
+  })
+}
